dc2ea7d3448edc1ec3a525bbe8d0dc5ef07fae5d
be techye <be.techye@gmail.com>
2023-11-06 14:00:53 +0545

    New changes

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 mongoose
   .connect('mongodb://localhost:27017/ser-pro-db')
-  .then(() => console.log('Database Connected!'));
+  .then(() => console.log('Database Connected successfully!'));
 module.exports = mongoose;
